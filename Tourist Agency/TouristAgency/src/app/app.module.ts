63b4f3ea7a16a3ac76bf428e632b4aa7ac9e10a0
Christian Bodenwinkler <c.bodenwinkler@gmx.at>
2020-10-19 10:21:40 +0200

    Angulare Day 1 Basic Setup Tourist Agency

---
+++ Tourist Agency/TouristAgency/src/app/app.module.ts
@@ -0,0 +1,28 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { HeroComponent } from './hero/hero.component';
+import { MainComponent } from './main/main.component';
+import { DestinationsComponent } from './destinations/destinations.component';
+import { AboutPageComponent } from './about-page/about-page.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    NavbarComponent,
+    HeroComponent,
+    MainComponent,
+    DestinationsComponent,
+    AboutPageComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ Tourist Agency/TouristAgency/src/app/destinations/destinations.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'destinations',
+  templateUrl: './destinations.component.html',
+  styleUrls: ['./destinations.component.sass']
+})
+export class DestinationsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
